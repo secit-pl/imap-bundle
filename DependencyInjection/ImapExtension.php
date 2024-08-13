1a3d91f987c322d20c29904dc2212aea96fa06e5
Flav B. <46626945+flavou45@users.noreply.github.com>
2024-08-13 17:21:23 +0200

    Update ImapExtension.php (#43)
    feature: fix deprecation of Extension

--- DependencyInjection/ImapExtension.php
+++ DependencyInjection/ImapExtension.php
@@ -9,8 +9,7 @@ use SecIT\ImapBundle\Connection\ConnectionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
